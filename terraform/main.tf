132f684ea07316872245129bebe6787ee70ba2e3
BinaryVixen899 <lealac@msn.com>
2022-08-07 19:16:36 -0700

    Now uses HTTPS and connects to 443 on api.pluralkit.me

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,14 @@ resource "fastly_service_compute" "api_kitsune_gay" {
     activate = true
     name = "api.kitsune.gay"
     comment = "api sewvice fow a cewtain focks"
-    version_comment = "Transitioning to using Terraform"
+    version_comment = "Changing Backend to use HTTPS"
     backend {
       address = "https://api.pluralkit.me"
     # DO NOT CHANGE THE NAME OR YOU WILL RECREATE THE BACKEND
       name = "Pluralkit"
       override_host = "api.pluralkit.me"
+      port = 443
+      use_ssl = true
     }
 
     domain {
