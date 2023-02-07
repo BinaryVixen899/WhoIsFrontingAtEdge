2b1183105777a335acc155e79ab67b6bc2035b4f
BinaryVixen899 <lealac@msn.com>
2023-02-07 05:25:17 +0000

    Ran cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 //! Adapted from the Default Compute@Edge template program.
 
-
-
 use fastly::http::{header, Method, StatusCode};
 use fastly::{mime, Error, Request, Response};
 
@@ -9,25 +7,20 @@ use fastly::{mime, Error, Request, Response};
 struct pkdata {
     // id: i32,
     // timestamp: i32,
-    members: Vec<pkmember>
-    
+    members: Vec<pkmember>,
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct pkmember {
     id: String,
-    name: String
+    name: String,
 }
-
-
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    
     // Define backend names
     const pluralkit_backend: &str = "pluralkit";
-    
-    
+
     // Filter request methods...
     match req.get_method() {
         // Allow GET and HEAD requests.
@@ -43,8 +36,7 @@ fn main(req: Request) -> Result<Response, Error> {
 
     // Pattern match on the path...
     match req.get_path().to_lowercase().as_str() {
-        // Make incoming string lowercase 
-         
+        // Make incoming string lowercase
 
         // If request is to the `/` path...
         "/" => {
@@ -55,59 +47,60 @@ fn main(req: Request) -> Result<Response, Error> {
                 .with_body(include_str!("welcome-to-compute@edge.html")))
         }
         "/fronting" => {
-            // Create a new request 
+            // Create a new request
             // Later let's turn this into a method/function
             let bereq = Request::get("https://api.pluralkit.me/v2/systems/rzwbg/fronters")
-            .with_pass(true)
-            .with_header("accept", "*/*")
-            .with_header("host", "api.pluralkit.me");
-            
+                .with_pass(true)
+                .with_header("accept", "*/*")
+                .with_header("host", "api.pluralkit.me");
+
             let beresp = bereq.send(pluralkit_backend);
-            
-            // Checking for Send Errors 
+
+            // Checking for Send Errors
             if let Err(e) = beresp {
                 // Log what we encountered
                 eprintln!("We've encountered a send error {}:", e);
-                // Clearly state it's not our fault 
-                return Ok(Response::from_status(StatusCode::BAD_GATEWAY))    
+                // Clearly state it's not our fault
+                return Ok(Response::from_status(StatusCode::BAD_GATEWAY));
             }
             // otherwise..
             let mut beresp = beresp.expect("To have handled an invalid send");
-            if !beresp.get_status().is_success()
-            {
-                eprintln!("Instead of giving us a 200, PK gave us a {}", beresp.get_status());
-                return Ok(Response::from_status(StatusCode::SERVICE_UNAVAILABLE))
-
+            if !beresp.get_status().is_success() {
+                eprintln!(
+                    "Instead of giving us a 200, PK gave us a {}",
+                    beresp.get_status()
+                );
+                return Ok(Response::from_status(StatusCode::SERVICE_UNAVAILABLE));
             }
-            
+
             let my_data = beresp.take_body_json::<pkdata>();
             if let Err(e) = my_data {
                 eprintln!("JSON PARSING FAILURE: {}", e);
-                return Ok(Response::from_status(StatusCode::SERVICE_UNAVAILABLE))
+                return Ok(Response::from_status(StatusCode::SERVICE_UNAVAILABLE));
             }
 
             let my_data = my_data.expect("To have handled a case in which we had bad JSON data");
-            
+
             // So now we just need to handle this differently depending on if its someone asking for json or not
-            // We can just destroy the reference otherwise because we don't rIeally care 
+            // We can just destroy the reference otherwise because we don't rIeally care
             let result = match req.get_header("Accept") {
                 Some(x) if x == "application/json" => Some(x),
                 _ => None,
             };
 
-           if result.is_some() {
-                // Send json of the fronting member back to the client 
+            if result.is_some() {
+                // Send json of the fronting member back to the client
                 Ok(Response::from_status(StatusCode::OK)
-                .with_body_json(&my_data.members[0].name).expect("We are able to parse the JSON back")
-                .with_content_type(mime::APPLICATION_JSON)
-                )
-           }
-
-           else {
+                    .with_body_json(&my_data.members[0].name)
+                    .expect("We are able to parse the JSON back")
+                    .with_content_type(mime::APPLICATION_JSON))
+            } else {
                 //Display who is fronting in a nice page
-               return Ok(Response::from_status(StatusCode::OK)
-               .with_content_type(mime::TEXT_HTML_UTF_8)
-               .with_body_text_html(format!(" <head> <meta charset=\"UTF-8\">
+                return Ok(Response::from_status(StatusCode::OK)
+                    .with_content_type(mime::TEXT_HTML_UTF_8)
+                    .with_body_text_html(
+                        format!(
+                            " <head> <meta charset=\"UTF-8\">
             <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
             <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
             <title>Document</title>
@@ -115,17 +108,18 @@ fn main(req: Request) -> Result<Response, Error> {
             <body>
             <p>\"The current fronter is: {alter}!\"</p>
             </body>
-            </html>", alter = my_data.members[0].name).as_str()))
-           }
-
-
+            </html>",
+                            alter = my_data.members[0].name
+                        )
+                        .as_str(),
+                    ));
+            }
         }
 
         // Catch all other requests and return a 404.
-        _ =>
-             Ok(Response::from_status(StatusCode::NOT_FOUND)
+        _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
             .with_content_type(mime::TEXT_HTML_UTF_8)
             // .with_body_text_html(format!("test {alter}", alter = "test").as_str()))
-            .with_body(include_str!("magikarp.html")))
+            .with_body(include_str!("magikarp.html"))),
     }
 }
